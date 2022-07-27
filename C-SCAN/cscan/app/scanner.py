7b1234b1a655973767dc30cbcced7b0291b7a8aa
Alexander Kabatov <57453479+akabatov@users.noreply.github.com>
2022-07-27 14:23:52 -0400

    Update scanner.py
    Created comments

--- C-SCAN/cscan/app/scanner.py
+++ C-SCAN/cscan/app/scanner.py
@@ -1,5 +1,3 @@
-from importlib.resources import path
-from tracemalloc import start
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By 
@@ -8,11 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from django.conf import settings
 
-
-PATH = r'C:\Users\alexander.kabatov\OneDrive - Gestion Vamcorp Ltee\Desktop\C-SCAN\chromedriver.exe'
+#Set the path for the driver and XPATHs for elements on a webpage 
+PATH = settings.BASE_DIR + '/' + 'chromedriver.exe'
 INPUT_XPATH = '//*[@id="panel"]/div[1]/input'
 TEST_XPATH = '//body/div[3]/div[4]/div[1]/div[2]/div[4]'
 
+#Class to create the 2 dimentional list and store information about containers 
 class Scan():
     global file_length 
 
@@ -20,31 +19,49 @@ class Scan():
         self.file = file
         self.info_arr = info_arr
 
+
+    #Function returns the path to the file that was uploaded by user
     def getPath(self):
         return settings.MEDIA_ROOT + "/" + self.file
     
+    #Getter funtion for self.file
     def getFile_Name(self):
         return self.file
 
+    #Function returns the global variable file_length 
+    #Input  --- NONE
+    #Output --- integer (file_length)
     def getFile_length(self):
         global file_length
         return file_length
-        
+
+    #Function gets rid of the commas and brackets and returns the 2D list
+    # Input  --- NONE
+    # Output --- list (2 dimentional list with container information)
     def getInfo_arr(self):
         result = (', '.join(l) for l in self.info_arr)
         return result
 
+    #Function sets the length of the 2D list to specified values
+    #Input  --- columns (amount of columns), rows (amount of rows)
+    #Output --- NONE
     def setLength(self, columns, rows):
         self.info_arr = [[0 for x in range(columns)] for x in range(rows)]
 
-    def setInfo_arr(self, object, pos, value):
-        self.info_arr[object][pos] = value
+    #Function puts a specified value into specified position of the 2D list
+    #Input  --- row (y position), column (x position), value (value to input)
+    #Output --- NONE
+    def setInfo_arr(self, row, column, value):
+        self.info_arr[row][column] = value
+
     
+    #Function reads gets the file, reads all the info and stores it in a list
+    #Input  --- NONE
+    #Output --- List 
     def read_file(self):
         global file_length
         container_list = []
         path = self.getPath()
-        print(self.getPath())
         with open(path, 'r') as f:
             line = f.readline()
             while  len(line) > 1:
@@ -54,26 +71,40 @@ class Scan():
         self.setLength(6, file_length)
         return container_list 
 
-    def scan_iteration(self):
 
+    #With each iteration function opens the browser, inputs the container ID, retrieves the information and stores it inside the 2 dimentional list
+    #Input  --- NONE
+    #Output --- NONE
+    def scan_iteration(self):
+        #Locate the WebDriver and open the webpage 
         driver = webdriver.Chrome(PATH)
         driver.get('https://sirius.searates.com/tracking/multitracking?')
         
+        #List of container ids from uploaded file
         container_list = self.read_file()
+
+        #Contianer index for 2D list
         container_number = 0
 
+        #for each id in the uploaded file 
         for id in container_list:
+
+            #Locate the input field, input ID and press ENTER
             search = driver.find_element(By.NAME, "container")
             search.send_keys(id)
             search.send_keys(Keys.RETURN)
 
+            #element loaded and data is available 
             try:
+                #Wait for 10 seconds if element loads
                 wait = WebDriverWait(driver, 10, poll_frequency=1)
                 element = wait.until_not(EC.presence_of_element_located((By.XPATH, TEST_XPATH)))
 
+                #Retrieve HTML of the page 
                 html = driver.page_source
                 soup = BeautifulSoup(html, 'html.parser')
 
+                #Find requiered information and store it in the list 
                 sealine = soup.find('p', {'class': 'container-id'}).get('data-sealine')
                 destination_info = soup.find('div', {'class': 'destination'})
                 children = destination_info.findChildren('div', recursive=False)
@@ -86,31 +117,23 @@ class Scan():
                 self.setInfo_arr(container_number, 4, start_date)
                 self.setInfo_arr(container_number, 5, end_date)
 
-                # container_info.append(id)
-                # container_info.append(soup.find('p', {'class': 'container-id'}).get('data-sealine'))
-                # destination_info = soup.find('div', {'class': 'destination'})
-                # children = destination_info.findChildren('div', recursive=False)
-                # container_info.append(children[0].text)
-                # container_info.append(children[1].text)
-                # container_info.append(soup.find('div', {'class': 'bl-start-date eta'}).text)
-                # container_info.append(soup.find('div', {'class': 'bl-finish-date etd'}).text)
-
+                #clear the search bar
                 search.clear()
 
+            #Element didn't load or no data available
             except:
+                #fill the list with 'NA'
                 self.setInfo_arr(container_number, 0, id)
                 self.setInfo_arr(container_number, 1, "NA")
                 self.setInfo_arr(container_number, 2, "NA")
                 self.setInfo_arr(container_number, 3, "NA")
                 self.setInfo_arr(container_number, 4, "NA")
                 self.setInfo_arr(container_number, 5, "NA")
-                # container_info.append(id)
-                # i = 0
-                # while i < 6:
-                #     container_info.append('NA')
-                #     i+=1
+
+                #clear the search bar
                 search.clear()
-            
+
+            #increment the y index in 2D array
             container_number += 1
     
 
